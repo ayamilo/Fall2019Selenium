64660996bdaba6dcffd012d0ef07df028d5a919c
ayamilo <ayamilo2000@gmail.com>
2020-03-01 12:32:58 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,16 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManagerException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+       //setup chromedriver
+//       WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
+        //create chromedriver object
+        ChromeDriver driver =new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
